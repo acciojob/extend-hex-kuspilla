4572e902ec8b2fe0c8b7ec788a747aa0bd67f505
Dasthu <118829965+kuspilla@users.noreply.github.com>
2023-11-22 07:55:34 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,12 @@
 const extendHex = (shortHex) => {
   // write your code here
+	let ans = "#";
+	for( let i of shortHex){
+		ans = ans + i;
+		ans = ans + i;
+	}
+	
+	return ans;
 };
 
 // Do not change the code below.
